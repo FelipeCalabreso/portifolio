02b1b86f95657e8dec21ef14a12f0568c0ea0698
J.Felipe <josefbn1304@gmail.com>
2026-04-23 19:03:22 -0300

    Add files via upload

---
+++ Tela inicial.js
@@ -0,0 +1,9 @@
+function ir1 (event){
+    window.location.href="https://github.com/FelipeCalabreso/Site-de-Produtos-online"
+}
+function ir2 (event){
+    window.location.href="https://batmasterson.itch.io/ntr"
+}
+function ir3 (event){
+    window.location.href="https://github.com/FelipeCalabreso/noat-medai-sistema"
+}
